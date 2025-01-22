37b3a9fa020fc840ae0e4c3985c010dee4cde418
debapriya2003 <121689840+debapriya2003@users.noreply.github.com>
2025-01-23 01:43:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.title("Anime Wallpaper App")
 
 # Function to fetch images from the API
 def fetch_images():
-    url = "https://api.waifu.pics/many/sfw/neko"
+    url = "https://api.waifu.pics/many/sfw/waifu"
     try:
         response = requests.get(url)
         if response.status_code == 200:
